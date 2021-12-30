bebeb3bd06a08b09cb4863acdcd91da9b6a7c996
Shawn Fox <sdfox1874@gmail.com>
2021-12-29 21:41:19 -0800

    Improved and added to TestSmartPointers.cpp

--- ContainerTests/TestSmartPointers.cpp
+++ ContainerTests/TestSmartPointers.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "TestSmartPointers.h"
 #include <memory>
+#include "Integer.h"
 
 TestSmartPointers::TestSmartPointers()
 {
@@ -11,7 +12,7 @@ TestSmartPointers::~TestSmartPointers()
 {
 }
 
-TEST_F(TestSmartPointers, UniquePointerTest)
+TEST_F(TestSmartPointers, TestUniquePtrRelease)
 {
   auto p = std::make_unique<int>(0);
   *p = 5;
@@ -23,3 +24,13 @@ TEST_F(TestSmartPointers, UniquePointerTest)
   GTEST_ASSERT_NE(temp, nullptr);
   GTEST_ASSERT_EQ(p, nullptr);
 }
+
+TEST_F(TestSmartPointers, TestUniquePtrObservers)
+{
+  auto p = std::unique_ptr<Integer>(new Integer(5));
+  EXPECT_TRUE(p);
+  auto p2 = p.get();
+  EXPECT_TRUE(p);
+  EXPECT_TRUE(p2);
+}
+
